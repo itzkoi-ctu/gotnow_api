e48b6c5f36749efb00d0128788860c7e84da0e11
itz-koi <longnguyen0397042@gmail.com>
2025-03-05 22:02:07 +0700

    deploy

--- src/main/java/com/itzkoictu/gotNow/GotNowApplication.java
+++ src/main/java/com/itzkoictu/gotNow/GotNowApplication.java
@@ -2,8 +2,11 @@ package com.itzkoictu.gotNow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.itzkoictu.gotNow.repository") // Đảm bảo đúng package
+
 public class GotNowApplication {
 
 	public static void main(String[] args) {
