01a413dae0b7d092420df9b2ad0d0a9756f052be
itz-koi <longnguyen0397042@gmail.com>
2025-03-27 10:19:48 +0700

    change BaseURL

--- src/main/java/com/itzkoictu/gotNow/security/config/WebSocketConfig.java
+++ src/main/java/com/itzkoictu/gotNow/security/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173") // Cho phép frontend kết nối
+                .setAllowedOrigins("https://gotnow-ui-yncs.vercel.app/") // Cho phép frontend kết nối
                 .addInterceptors(new WebSocketAuthInterceptor())
                 .withSockJS();
     }
